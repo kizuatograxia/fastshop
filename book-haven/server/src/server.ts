4ac304ebd70a0b576d2e23930f3f717dcf582663
kizuatograxia <hedgehogdilemma1851@gmail.com>
2026-02-19 04:18:30 -0300

    fix(deploy): restore path import and remove unused backend code

--- book-haven/server/src/server.ts
+++ book-haven/server/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes';
+import path from 'path';
+
+// import authRoutes from './routes/authRoutes';
 import adminRoutes from './routes/adminRoutes';
 import apiRoutes from './routes/apiRoutes';
 
